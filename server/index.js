a3730f9829a8af01062392a5472748ccf10a9ab0
Fahyvor <favourokafor30@gmail.com>
2023-07-27 15:35:23 +0100

    CORS errors

--- server/index.js
+++ server/index.js
@@ -18,14 +18,24 @@ require('dotenv').config()
 
 app.use(cors())
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin *', 'https://mern-stack-app-g1gi.vercel.app');
-    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173/');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin *', 'https://mern-stack-app-g1gi.vercel.app');
+//     // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173/');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+//   });
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Request-Headers', '*');
+  if (req.method === "OPTIONS") {
+    res.header('Access-Control-Allow-Methods', '*');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 
   app.get('/', (req, res) => {
