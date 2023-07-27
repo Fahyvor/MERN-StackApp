2df0fc90ae83ba99038fc8890cd22fa5f16b65d2
Fahyvor <favourokafor30@gmail.com>
2023-07-27 15:46:06 +0100

    CORS errors

--- server/index.js
+++ server/index.js
@@ -5,37 +5,25 @@ app.use(express.json())
 const connectDB = require('./db/connect')
 require('dotenv').config()
 
-// const allowedOrigins = ['https://mern-stack-app-g1gi-qehof4t60-fahyvor.vercel.app'];
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if (allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   }
-// }));
-
 app.use(cors())
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin *', 'https://mern-stack-app-g1gi.vercel.app');
-//     // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173/');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     next();
-//   });
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://mern-stack-app-g1gi.vercel.app');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Request-Headers', '*');
-  if (req.method === "OPTIONS") {
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    return res.status(200).json({});
-  }
   next();
 });
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://mern-stack-app-g1gi.vercel.app');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.header('Access-Control-Request-Headers', '*');
+//   if (req.method === "OPTIONS") {
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
 
 
   app.get('/', (req, res) => {
