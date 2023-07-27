66a45aabcc5399dfdf906709c8bf5b8f35afbca8
Fahyvor <favourokafor30@gmail.com>
2023-07-27 15:23:26 +0100

    CORS errors

--- client/src/components/axios.js
+++ client/src/components/axios.js
@@ -2,7 +2,10 @@ import axios from 'axios'
 
 const instance = axios.create({
     baseURL: "https://mern-stack-app-delta.vercel.app/",
-    withCredentials: true
+    withCredentials: false,
+    params: {
+        access_token: SECRET_TOKEN
+    },
 });
 
 export default instance;
