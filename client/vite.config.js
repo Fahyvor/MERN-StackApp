ec1cdcf0cf438bcf987f9a290a02fd0f5a211887
Fahyvor <favourokafor30@gmail.com>
2023-07-27 15:55:34 +0100

    CORS errors

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api/v1/auth': 'https://mern-stack-app-delta.vercel.app/'
+    }
+  }
   plugins: [react()],
 })
